7fe299495eadef57db5a33099f9233b02e5f4d6c
aysam100 <61271432+aysam100@users.noreply.github.com>
2022-07-15 14:35:44 +0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,34 @@
 import React, { useState } from "react";
 import "./App.css";
 import Home from './Home'
+import { Messages } from "./messages"
 
 function App() {
-  return (
+  const [isLogged, setIsLogged] = useState(false)
+  const [password, setPassword] = useState('')
+  const handleLogin = () => {
+    if (password === '123')
+      setIsLogged(true)
+  }
+  return (isLogged ? 
+    <div className="App Loginpage">
+        <div class="login-box">
+          <h2>{Messages.loginHeader}</h2>
+          <form>
+            <div class="user-box">
+              <input type="password" name="" required="" onChange={(e) => setPassword(e.target.value)}/>
+              <label>{Messages.loginCode}</label>
+            </div>
+            <a onClick={() => handleLogin()}>
+              <span></span>
+              <span></span>
+              <span></span>
+              <span></span>
+              {Messages.submit}
+            </a>
+          </form>
+        </div> 
+    </div> :
     <div className="App">
       <Home />
     </div>
